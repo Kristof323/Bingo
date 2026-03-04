472dd0c0dbc1f863861d25fbda8af6f1b48fce6d
Kristof323 <kristofcsapo@gmail.com>
2026-03-04 08:43:30 +0100

    kartya keszites

--- Bingo/MainWindow.xaml.cs
+++ Bingo/MainWindow.xaml.cs
@@ -1,28 +1,74 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace Bingo
+namespace BingoGUI
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly TextBox[,] _cells = new TextBox[5, 5];
+        private readonly Random _rng = new Random();
+
         public MainWindow()
         {
             InitializeComponent();
+            BuildCardInputs();
+            SetCenterX();
+        }
+
+        private void BuildCardInputs()
+        {
+            CardGrid.Children.Clear();
+            for (int r = 0; r < 5; r++)
+                for (int c = 0; c < 5; c++)
+                {
+                    var tb = new TextBox
+                    {
+                        Width = 44,
+                        Height = 28,
+                        Margin = new Thickness(4),
+                        TextAlignment = TextAlignment.Center,
+                        VerticalContentAlignment = VerticalAlignment.Center
+                    };
+                    _cells[r, c] = tb;
+                    CardGrid.Children.Add(tb);
+                }
+        }
+
+        private void SetCenterX()
+        {
+            var center = _cells[2, 2];
+            center.Text = "X";
+            center.IsReadOnly = true;
+            center.Focusable = false;
+        }
+
+        private void GenerateButton_Click(object sender, RoutedEventArgs e)
+        {
+            for (int c = 0; c < 5; c++)
+            {
+                int start = c * 15 + 1;
+                var values = Enumerable.Range(start, 15)
+                    .OrderBy(_ => _rng.Next())
+                    .Take(5)
+                    .ToArray();
+
+                for (int r = 0; r < 5; r++)
+                {
+                    if (r == 2 && c == 2) continue;
+                    _cells[r, c].IsReadOnly = false;
+                    _cells[r, c].Text = values[r].ToString(CultureInfo.InvariantCulture);
+                }
+            }
+            SetCenterX();
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
         }
     }
 }
